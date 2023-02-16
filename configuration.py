ef3f0ceea77a7ac3d56daeede6b8be1b4da29344
FedorTrukhachev <122970430+FedorTrukhachev@users.noreply.github.com>
2023-02-16 16:41:25 +0300

    my_first_commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://e6c566e5-5e86-46ce-ba21-ea3563314642.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
